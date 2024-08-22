7d40e3322ac1e8643221048dc0ad3699ef7488f4
unknown <mahmoudmaged536@gmail.com>
2024-08-22 20:19:38 +0300

    The Assignment contain Day2 &Day3 seince they share a few common things

---
+++ Controllers/InstructorsController.cs
@@ -0,0 +1,22 @@
+﻿using ITI_MVC_Assingment_D2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITI_MVC_Assingment_D2.Controllers
+{
+    public class InstructorsController : Controller
+    {
+        ApplicationDbContext _context =new ApplicationDbContext();
+        public IActionResult Index()
+        {
+
+            var instructors = _context.Instructors.ToList();
+            return View(instructors);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var instructor = _context.Instructors.Find(id);
+            return View(instructor);
+        }
+    }
+}
